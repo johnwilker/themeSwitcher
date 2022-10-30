99cffbcaf6ea1991f4dd1b95d4edf143385ba6e7
John Wilker <johnwilker2@gmail.com>
2022-10-29 23:26:47 -0300

    social links updated and git icon change color

--- scripts.js
+++ scripts.js
@@ -1,14 +1,10 @@
-// ALTERADOR DE CORES FUNCIONANDO
 const input = document.querySelector(".input")
 const body = document.querySelector("body")
 const theme = window.localStorage.getItem("theme")
-const gitIconColor = document.querySelector("#gitIcon")
-
 
 
 const themeSwitcher = () => {
      body.classList.toggle("light")
-     gitIconColor.setAttribute("fill", '#000')
 }
 
  input.onchange = themeSwitcher
